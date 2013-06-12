5247a4f96d877879aecd0a36526ebee5076c09f1
DiegoSeC <diego_sc_89@hotmail.com>
2013-06-11 20:40:27 -0500

    borrando migraciones

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130610203644) do
+ActiveRecord::Schema.define(:version => 20130610175037) do
 
   create_table "assistances", :force => true do |t|
     t.integer  "user_id"
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(:version => 20130610203644) do
     t.string   "name"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
-    t.integer  "band_style_id"
     t.integer  "user_id"
+    t.integer  "band_style_id"
   end
 
   create_table "comments", :force => true do |t|
